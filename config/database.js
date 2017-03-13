ebc045e22d94c7c17c080fe672d5a91bd3502136
maratnek <maratn566ek@gmail.com>
2017-03-13 09:42:38 +0300

    linked db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
 	// database: 'mongodb://localhost:27017/meanauth',
-	database: 'mongodb://localhost/meanauth',
+	// database: 'mongodb://localhost/meanauth',
+
+	database: 'mongodb://kent:kent@ds129030.mlab.com:29030/meanauthapp_kentavr',
 	secret: 'yousecret'
 }
